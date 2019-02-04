d3301fc187cce111e08f92601e301a99653ecfd9
programinurdu <aasdb@progd23.com>
2019-02-04 17:27:02 +0000

    Add project files.

---
+++ DropDownEnableDisable/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DropDownEnableDisable.Models
+{
+    public class StudentModel
+    {
+        [Display(Name = "Full Name:")]
+        [Required(ErrorMessage = "Full Name is required.")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Are you exempt from the fee?:")]
+        [UIHint("YesNoDropDownList")]
+        public string IsExempted { get; set; }
+
+        [Display(Name = "Comment if Exempt:")]
+        public string CommentIfExempt { get; set; }
+    }
+}
---
+++ DropDownEnableDisable/Scripts/Site.js
@@ -0,0 +1,9 @@
+﻿function enabledisabletextbox(val) {
+    if (val === 'Yes' || val === '') {
+        document.getElementById('CommentIfExempt').disabled = true;
+        document.getElementById('CommentIfExempt').value = "";
+    }
+    else {
+        document.getElementById('CommentIfExempt').disabled = false;
+    }
+}
